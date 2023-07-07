f8f2e4b5dc7e399042de28399f90720e05a63732
Elliott Prechter <elliott.prechter@gmail.com>
2023-07-07 14:51:28 -0400

    added note about sourcing the script

--- tools/setup_cross_env.sh
+++ tools/setup_cross_env.sh
@@ -1,3 +1,5 @@
+# note: this file must be executed via "source ./setup_cross_env.sh
+# that way, the exports appear in the calling environment (otherwise, they'll be script-local)
 export PREFIX="$HOME/opt/cross64"
 export TARGET=x86_64-elf
 export PATH="$PREFIX/bin:$PATH"
