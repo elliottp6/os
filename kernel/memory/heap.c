2b61bab528fa106ffdefbcbf3de418fd4bf692cf
Elliott Prechter <elliottp@ewaves.com>
2023-07-11 14:26:21 -0400

    started on heap_allocate

--- kernel/memory/heap.c
+++ kernel/memory/heap.c
@@ -17,12 +17,12 @@ typedef struct heap_header { // note: we could make the free_blocks array dynami
 // [2] => free (MIN_BLOCK_SIZE << 2) byte block
 // ...
 void heap_init( void *heap, size_t size ) {
-    // grab header
+    // heap header lives @ the start of the heap
     heap_header_t *heap_header = (heap_header_t*)heap;
 
-    // skip past the heap header
+    // blocks live past the heap header
     heap+= sizeof( heap_header_t );
-    size -= sizeof( heap_header_t );
+    size-= sizeof( heap_header_t );
 
     // populate free list (large..small blocks)
     for( size_t i = NUM_BLOCK_SIZES - 1; i >= 0; i++ ) {
@@ -47,16 +47,41 @@ void heap_init( void *heap, size_t size ) {
     }
 }
 
-/*
-alloc:
-(1) if we have block of size, use block
-    if we have block of > 2*size, split that block and goto 1
-    otherwise, ERROR_NOMEM, or just PANIC
-(2) fix block forward pointers (each block points to next & prev block of same size)
-(3) fix root block table
+// (1) if we have block of size, use block
+//     if we have block of > 2*size, split that block and goto 1
+//     otherwise, ERROR_NOMEM, or just PANIC
+// (2) fix block forward pointers (each block points to next & prev block of same size)
+// (3) fix root block table
+void* heap_allocate( void* heap, size_t size ) {
+    // adjust object size to include header
+    size+= sizeof( block_header_t );
 
-free:
-(1) address - N bytes to get block header
-(2) set prev block to point to next block of same size (i.e. removing from list)
-(3) if this block is earlier in list than root block's pointer, update root block's pointer
-*/
+    // heap header lives @ the start of the heap
+    // we can just case it directly to a list of free blocks for faster access
+    block_header_t *free_blocks = (block_header_t*)heap;
+
+    // look for the smallest free block that will contain this object w/ its header
+    for( size_t i = 0, block_size = MIN_BLOCK_SIZE; i < NUM_BLOCK_SIZES; i++, block_size<<=1 ) {
+        block_header_t *block_header = free_blocks[i];
+        
+        // skip block if it's missing, or if it's too small
+        if( NULL == block_header || (size > block_size) ) continue;
+
+        // ok, we have a block that's big enough --> should we split it into two smaller blocks?
+        // TODO
+
+        // if we don't need to split, allocate
+        // TODO
+    }
+
+    // not enough free contiguous space to allocate!
+    return NULL;
+}
+
+// (1) address - N bytes to get block header
+// (2) set prev block to point to next block of same size (i.e. removing from list)
+// (3) if this block is earlier in list than root block's pointer, update root block's pointer
+// what about also maintaining a free list? may want to redesign...
+void heap_free( void* heap, void* p ) {
+    // TODO
+}
--- kernel/memory/heap.h
+++ kernel/memory/heap.h
@@ -2,5 +2,5 @@
 #include <stddef.h>
 
 void heap_init( void *heap, size_t size );
-//void* heap_alloc( void* heap, size_t size );
-//void heap_free( void* heap, void* p );
+void* heap_allocate( void* heap, size_t size );
+void heap_free( void* heap, void* p );
