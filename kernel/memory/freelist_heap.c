265e1a2619516d0c33a7f1b8b2328e5b25613205
Elliott Prechter <elliottp@ewaves.com>
2023-07-18 14:06:53 -0400

    implemented freelist_heap_init

--- kernel/memory/freelist_heap.c
+++ kernel/memory/freelist_heap.c
@@ -1,20 +1,32 @@
 #include "freelist_heap.h"
 
-typedef struct free_block {
-    circular_list_node_t node;
-    size_t size;
-    uint8_t data[];
-} free_block;
+// alignment must be a power of 2 (or else undefined behavior)
+static size_t upalign_offset( void *address, size_t alignment_minus_1 ) {
+    uintptr_t current = (size_t)address;
+    uintptr_t next = (current + alignment_minus_1) & ~alignment_minus_1;
+    return next - current;
+}
 
 void freelist_heap_init( freelist_heap *heap, void* start, size_t size ) {
+    // set fields for heap
     heap->start = start;
     heap->size = size;
 
-    // allocate root block
-    // TODO
+    // check if we can fit a root block in heap w/ data_size >= 1
+    size_t start_offset = upalign_offset( start, sizeof( size_t ) - 1 );
+    size_t data_offset = start_offset + sizeof( freelist_block );
+    if( size <= data_offset ) {
+        heap->root = NULL;
+        return;
+    }
+
+    // initialize root block
+    freelist_block *root = heap->root = (freelist_block*)(start + start_offset);
+    circular_list_init( (circular_list_node_t*)root );
+    root->data_size = size - data_offset; // this is always >= 1
 }
 
-static free_block *find_free_block( freelist_heap *heap ) {
+static freelist_block *find_free_block( freelist_heap *heap ) {
     return NULL;
 }
 
--- kernel/memory/freelist_heap.h
+++ kernel/memory/freelist_heap.h
@@ -5,11 +5,17 @@
 #include <stdbool.h>
 #include "circular_list.h"
 
+typedef struct freelist_block {
+    circular_list_node_t node;
+    size_t data_size;
+} freelist_block;
+
 typedef struct freelist_heap {
     void* start;
-    size_t size
+    size_t size;
+    freelist_block *root;
 } freelist_heap;
 
-//void freelist_heap_init( freelist_heap *heap, void* start, size_t size );
-//void* freelist_heap_allocate( freelist_heap* heap, size_t size );
-//void freelist_heap_free( freelist_heap* heap, void* ptr );
+void freelist_heap_init( freelist_heap *heap, void* start, size_t size );
+void* freelist_heap_allocate( freelist_heap* heap, size_t size );
+void freelist_heap_free( freelist_heap* heap, void* ptr );
