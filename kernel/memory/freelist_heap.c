896958211fab6dd9a615df760ffacabcb184e3bf
Elliott Prechter <elliottp@ewaves.com>
2023-07-18 18:08:40 -0400

    providing typedefs

--- kernel/memory/freelist_heap.c
+++ kernel/memory/freelist_heap.c
@@ -1,7 +1,11 @@
 #include "freelist_heap.h"
 
-// a memory block should be at least big enough to hold the block header and a size_t x
-#define MIN_BLOCK_SIZE (sizeof(freelist_block) + sizeof(size_t))
+typedef freelist_used_block_t used_block_t;
+typedef freelist_free_block_t free_block_t;
+typedef freelist_heap_t heap_t;
+typedef circular_list_node_t node_t;
+
+#define MIN_FREE_BLOCK_SIZE (sizeof( free_block_t ) + sizeof( size_t ))
 
 // alignment must be a power of 2 (or else undefined behavior)
 static size_t offset_for_forward_align( size_t x, size_t alignment_minus_1 ) {
@@ -9,61 +13,71 @@ static size_t offset_for_forward_align( size_t x, size_t alignment_minus_1 ) {
     return next - x;
 }
 
-void freelist_heap_init( freelist_heap *heap, void* start, size_t size ) {
-    // set fields for heap
+void freelist_heap_init( heap_t *heap, void *start, size_t size ) {
+    // initialize heap
     heap->start = start;
     heap->size = size;
     heap->root.block_size = 0;
-    circular_list_init( (circular_list_node_t*)&heap->root );
+    circular_list_init( (node_t*)&heap->root );
 
-    // check if we have enough space for root block
+    // check if we have enough space for root free block
     size_t offset = offset_for_forward_align( (size_t)start, sizeof( size_t ) - 1 );
-    if( size < offset + MIN_BLOCK_SIZE ) return;
+    if( size < offset + MIN_FREE_BLOCK_SIZE ) return;
 
-    // insert first block
-    freelist_block *block = (freelist_block*)(start + offset);
-    circular_list_insert_after( (circular_list_node_t*)&heap->root, (circular_list_node_t*)block );
-    block->block_size = size - offset;
+    // insert first free block
+    free_block_t *free_block = (free_block_t*)(start + offset);
+    circular_list_insert_after( (node_t*)&heap->root, (node_t*)free_block );
+    free_block->block_size = size - offset;
 }
 
-static bool block_is_big_enough( circular_list_node_t *block_node, void *min_block_size ) {
-    freelist_block *block = (freelist_block*)block_node;
-    return block->block_size >= *(size_t*)min_block_size;
+static bool free_block_is_big_enough( node_t *free_block_node, void *min_free_block_size ) {
+    free_block_t *free_block = (free_block_t*)free_block_node;
+    return free_block->block_size >= *(size_t*)min_free_block_size;
 }
 
-static freelist_block *find_big_enough_block( freelist_heap *heap, size_t min_block_size ) {
-    return (freelist_block*)circular_list_find( (circular_list_node_t*)&heap->root, block_is_big_enough, &min_block_size );
+static free_block_t *find_big_enough_free_block( heap_t *heap, size_t min_free_block_size ) {
+    return (free_block_t*)circular_list_find( (node_t*)&heap->root, free_block_is_big_enough, &min_free_block_size );
 }
 
-void *freelist_heap_allocate( freelist_heap *heap, size_t size ) {
-    // min_block_size is size w/ the block header, aligned to the pointer size
-    size_t min_block_size = offset_for_forward_align( size + sizeof( freelist_block ), sizeof( size_t ) - 1 );
+void *freelist_heap_allocate( heap_t *heap, size_t size ) {
+    // min_block_size is size w/ the used block header, aligned to the pointer size
+    size_t min_block_size = offset_for_forward_align( size + sizeof( used_block_t ), sizeof( size_t ) - 1 );
 
-    // see if we can find a block that's big enough
-    freelist_block *block = find_big_enough_block( heap, min_block_size );
-    if( NULL == block ) return NULL;
+    // see if we can find a free block that's big enough
+    free_block_t *free_block = find_big_enough_free_block( heap, min_block_size );
+    if( NULL == free_block ) return NULL;
 
-    // split block (if possible)
-    size_t free_space_size = block->block_size - min_block_size;
-    if( free_space_size >= MIN_BLOCK_SIZE ) {
+    // if there's enough free space in the block: split it
+    size_t free_space_size = free_block->block_size - min_block_size;
+    if( free_space_size >= MIN_FREE_BLOCK_SIZE ) {
         // shrink block to its minimum size
-        block->block_size = min_block_size;
+        free_block->block_size = min_block_size;
 
-        // initialize new free block
-        freelist_block *new_block = block + min_block_size;
-        new_block->block_size = free_space_size;
+        // initialize new free block right after this block
+        free_block_t *new_free_block = free_block + min_block_size;
+        new_free_block->block_size = free_space_size;
 
-        // replace block w/ the new free block on the free list
-        circular_list_replace( (circular_list_node_t*)block, (circular_list_node_t*)new_block );
+        // replace block with the new free block
+        circular_list_replace( (node_t*)free_block, (node_t*)new_free_block );
     } else {
         // remove block from the free list
-        circular_list_remove( (circular_list_node_t*)block )
+        circular_list_remove( (node_t*)free_block );
     }
 
+    // convert free_block into used_block
+    used_block_t* used_block = (used_block_t*)free_block;
+    used_block->block_size = free_block->block_size;
+
     // return pointer to the block's data
-    return block + sizeof( freelist_block );
+    return used_block + sizeof( used_block_t );
 }
 
-void freelist_heap_free( freelist_heap* heap, void* ptr ) {
-    // TODO
+void freelist_heap_free( heap_t* heap, void* ptr ) {
+    // get block & block_size
+    //used_block_t *used_block = (used_block_t*)(ptr - sizeof(used_block_t));
+    //size_t block_size = used_block->block_size;
+
+    // TODO: insert into freelist at correct memory location
+
+    // TODO: defragement adjacent blocks
 }
--- kernel/memory/freelist_heap.h
+++ kernel/memory/freelist_heap.h
@@ -5,17 +5,24 @@
 #include <stdbool.h>
 #include "circular_list.h"
 
-typedef struct freelist_block {
+// TODO: all of this can be moved into the implementation file
+// we can instead just put the 'freelist_heap' header object
+// directly into the beginning of the heapspace itself
+typedef struct freelist_used_block {
+    size_t block_size;
+} freelist_used_block_t;
+
+typedef struct freelist_free_block {
     circular_list_node_t node;
     size_t block_size;
-} freelist_block;
+} freelist_free_block_t;
 
 typedef struct freelist_heap {
     void *start;
     size_t size;
-    freelist_block root;
-} freelist_heap;
+    freelist_free_block_t root;
+} freelist_heap_t;
 
-void freelist_heap_init( freelist_heap *heap, void *start, size_t size );
-void *freelist_heap_allocate( freelist_heap *heap, size_t size );
-void freelist_heap_free( freelist_heap *heap, void *ptr );
+void freelist_heap_init( freelist_heap_t *heap, void *start, size_t size );
+void *freelist_heap_allocate( freelist_heap_t *heap, size_t size );
+void freelist_heap_free( freelist_heap_t *heap, void *ptr );
